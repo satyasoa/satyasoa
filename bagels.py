61b05f76d58cecf5233ace2ca7e02f909d28450f
satyasoa <146239802+satyasoa@users.noreply.github.com>
2024-01-05 22:16:22 +0530

    Update bagels.py

--- bagels.py
+++ bagels.py
@@ -1,72 +1,64 @@
 import os, random
-from datetime import datetime
 
 
-def get_month_and_date(year, day_of_year):
-    date = datetime.strptime(f'{year} {day_of_year}', '%Y %j')
-    month = date.strftime('%b')  # Month in three letter
-    day = date.strftime('%d')    # Date
-    
-    return f'{month} {day}'
+def generate_secret_number() -> str:
+    return str((random.sample(range(100, 1000), 1))[0])
 
-def getBirthdays(input_size) -> None:
-    dateList = [random.randint(1,365) for _ in range(input_size)]
-    random_birthdays = [get_month_and_date(2023,i) for i in dateList]
-    
-    return random_birthdays
-        
-
-def getMatch(birthday_list) -> bool:
-    find_dup_bday = list(set(birthday_list))
-    
-    if len(find_dup_bday) < len(birthday_list):
+def provide_hint(guess, random_num) -> bool:
+    if guess == random_num:
+        print("You got it!")
         return True
+    
+    clues = []
+    for i in range(3):
+        if guess[i] == str(random_num)[i]:
+            clues.append("Fermi")
+        elif guess[i] in str(random_num):
+            clues.append("Pico")
+    
+    if not clues:
+        print("Bagels")
+    else:
+        print(" ".join(clues))
     
     return False
     
-def main():
-    input_size = int(input("How many birthdays shall I generate? (Max 100)\n: "))
-    while True:
-        if input_size <= 100 and input_size > 0:
-            break
+def play_game() -> None:
+    random_num = generate_secret_number()
+    print(f"Generated number: {random_num}")
+    print("I have thought up a number.\nYou have 10 guesses to get it.")
+    for i in range(1,11):
+        current_guess = input(f"Guess #{i}\n> ")
+        if len(current_guess) != 3:
+            print("You can only input 3 digit numbers, You current chance was lost")
         else:
-            input_size = int(input("Maximum allowed input size is between 1 and 100. \nHow many birthdays shall I generate?\n: "))
+            if provide_hint(current_guess, random_num) == True:
+                break;
+            elif provide_hint(current_guess, random_num) == False and i == 10:
+                print(f"The correct number was {random_num}\n")
     
-    print(f"Here are {input_size} birthdays:")
-    tempList = getBirthdays(input_size)
-    for i in tempList:
-        print(i,end=" ")
-    print() # Change line
-    
-    if getMatch(tempList) == True:
-        print("Multiple People share same birthday in current simulation")
-    else:
-        print("In this simulation, everyone has unique birthdays.\n")
-    
-    print()
-    input(f"Generating {input_size} random birthdays 100,000 times...\nPress Enter to begin...")
-    print()
-    matching_list = []
-    for i in range(0,100000):
-        if i % 10000 == 0:
-            print(f"{i} simulations run...")
-        matching_list.append(getMatch(getBirthdays(input_size)))
-    print("100000 simulations run.")
-    print(f"Out of 100,000 simulations of {input_size} people, there was a matching birthday in that group {matching_list.count(True)} times.")
-    print(f"This means that 23 people have a {matching_list.count(True)/len(matching_list)*100} % chance of having a matching birthday in their group.")
-    print(f"That's probably more than you would think!")
 
 if __name__ == "__main__":
-    os.system("clear") # clear terminal
-    print("Birthday Paradox, by satya siddha dhal samanta (2241004243)")
+    os.system("clear") # clear the terminal window
+    print('''
+Bagels, a deductive logic game.
+By SATYA SIDDHA DHAL SAMANTA (2241004243)
+I am thinking of a 3-digit number. Try to guess what it is.
+Here are some clues:
+When I say: \t That means:
+Pico \t\t One digit is correct but in the wrong position.
+Fermi \t\t One digit is correct and in the right position.
+Bagels \t\t No digit is correct.
+        ''')
+    input("Press enter to begin...")
     while True:
-        main()
+        play_game()
         play_again = input("Do you want to play again? (yes or no)\n: ")
         if play_again == "yes":
             continue
         elif play_again == "no":
-            print("Thanks for playing....")
+            print("Thanks for playing!")
             exit(0)
         else:
-            print("Invalid choice, exitting....")
-            exit(1)
+            print("Invalid choice, Exitting......")
+            exit(0)
